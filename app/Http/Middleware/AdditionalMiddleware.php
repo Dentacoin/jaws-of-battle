14b2f82936a889f8925c8bad13d5744d51be30a4
unknown <miroslav.nedelchev@dentacoin.com>
2020-07-22 07:25:33 +0300

    add ads file and add x-frame header deny

--- app/Http/Middleware/AdditionalMiddleware.php
+++ app/Http/Middleware/AdditionalMiddleware.php
@@ -25,6 +25,7 @@ class AdditionalMiddleware
         $response = $next($request);
         $response->headers->set('Referrer-Policy', 'no-referrer');
         $response->headers->set('X-XSS-Protection', '1; mode=block');
+        $response->headers->set('X-Frame-Options', 'DENY');
         return (new App\Http\Controllers\Controller())->minifyHtml($response);
     }
 }
