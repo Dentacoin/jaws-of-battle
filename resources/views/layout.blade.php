2e424f3e07ed67eeecb9933d1caff70cd704daa3
unknown <miroslav.nedelchev@dentacoin.com>
2020-11-27 10:33:52 +0200

    setup time when to show the holiday banner

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -125,7 +125,7 @@
         </div>
     </footer>
 
-    @if(empty($_COOKIE['hide-holiday-calendar-banner']) && strtotime('2020/12/01 00:00:00') < time())
+    @if((empty($_COOKIE['hide-holiday-calendar-banner']) && strtotime('2020/12/01 00:00:00') < time()) || !empty($_COOKIE['test-holiday-calendar-banner']))
         <div class="bottom-fixed-promo-banner fs-0">
             <a href="javascript:void(0);" class="close-banner">×</a>
             <a href="https://dentacoin.com/holiday-calendar/2020" target="_blank">
