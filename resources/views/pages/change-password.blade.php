93c6f0b00b7abebf819f66649fd2b296dd6640f1
unknown <miroslav.nedelchev@dentacoin.com>
2021-08-11 10:08:05 +0300

    test forgotten passsword

--- resources/views/pages/change-password.blade.php
+++ resources/views/pages/change-password.blade.php
@@ -4,10 +4,10 @@
         <figure itemscope="" itemtype="http://schema.org/ImageObject" class="padding-top-10">
             <img src="/assets/uploads/dentacare-jaws-of-battle-logo.png" class="width-100 max-width-350" alt="Jaws of battle logo">
         </figure>
-        <div class="form-container color-white padding-top-50 padding-bottom-50 max-width-600 padding-left-15 padding-right-15">
+        <div class="form-container color-white padding-top-50 padding-bottom-50 max-width-600 margin-0-auto padding-left-15 padding-right-15">
             <form class="change-password-form" action="{{ route('submit-change-password') }}">
-                <h1 class="fs-44 lato-black">PASSWORD RECOVER</h1>
-                <div class="fs-26 padding-bottom-25">Please enter your new password.</div>
+                <h1 class="text-center fs-44 lato-black">PASSWORD RECOVER</h1>
+                <div class="text-center fs-26 padding-bottom-25">Please enter your new password.</div>
                 <div class="padding-bottom-15">
                     <input type="password" id="password" name="password" placeholder="Password:"/>
                 </div>
